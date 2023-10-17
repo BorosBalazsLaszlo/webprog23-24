2bb6e0496b8c0f9f7224a57ef0d3679ad23015e1
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2023-10-17 09:38:31 +0200

    Add files via upload

---
+++ dolgozat_10_17/javito.js
@@ -0,0 +1,7 @@
+document.getElementById('container1').style.backgroundColor ='#12406A';
+
+document.getElementById('item1').style.backgroundColor = '#EB8F90';
+document.getElementById('item2').style.backgroundColor = '#FFB471';
+document.getElementById('item3').style.backgroundColor = '#ADBED2';
+
+document.body.style.backgroundColor = '#CCFFFF';
