3d68a3b06d7f49a258472682985c04a83709959b
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2023-12-17 22:12:14 +0100

    stopper ✅

--- memoria_jatek/memoria.js
+++ memoria_jatek/memoria.js
@@ -3,6 +3,10 @@ let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 
+
+let seconds = 0;
+const stopperDisplay = document.getElementById('stopper-display');
+
 function Flip() {
     if (lockBoard) return;
     if (this === firstCard) return;
@@ -28,11 +32,7 @@ function Match() {
     }
 }
 
-function disableCard() {
-    firstCard.classList.add('hidden');
-    secondCard.classList.add('hidden');
-    Reset();
-}
+
 
 function unFlip() {
     lockBoard = true;
@@ -43,23 +43,61 @@ function unFlip() {
     }, 1500);
 }
 
+function disableCard() {
+    setTimeout(() => {
+        firstCard.classList.add('hidden');
+        secondCard.classList.add('hidden');
+        firstCard.style.opacity = 0;
+        secondCard.style.opacity = 0;
+        Reset();
+    }, 800);
+}
+
 function Reset() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
 }
 
 function checkWin() {
-    const hiddenCards = document.querySelectorAll('.hidden');
-    if (hiddenCards.length === kartyak.length) {
-        alert('Gratulálok, minden kártya eltűnt!');
-    }
+    setTimeout(() => {
+        const hiddenCards = document.querySelectorAll('.hidden');
+        if (hiddenCards.length === kartyak.length) {
+            alert('Gratulálok, minden kártya eltűnt!\nIdő: '  + seconds + ' másodperc');
+            restartGame();
+        }
+    }, 800);
 }
 
-(function shuffle() {
+function restartGame() {
+    kartyak.forEach(kartya => {
+        kartya.classList.remove('hidden', 'flip');
+        kartya.style.opacity = 1;
+    });
+
+    shuffleCards();
+
+    lockBoard = false;
+
+    seconds = 0;
+}
+
+function shuffleCards() {
     kartyak.forEach(kartya => {
         let random = Math.floor(Math.random() * 12);
         kartya.style.order = random;
     });
-})();
+}
 
-kartyak.forEach(kartya => kartya.addEventListener('click', Flip));
+(function initGame() {
+    shuffleCards();
+    kartyak.forEach(kartya => kartya.addEventListener('click', Flip));
+
+    setInterval(() => {
+        seconds++;
+
+        const displayMinutes = Math.floor(seconds / 60);
+        const displaySeconds = seconds % 60;
+
+        stopperDisplay.textContent = `Idő: ${displayMinutes} perc ${displaySeconds} másodperc`;
+    }, 1000); 
+})();
