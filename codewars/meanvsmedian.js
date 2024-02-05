05f83ab1c8d90ba0b52cd430cf3a81e108408dd1
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2024-02-05 13:23:42 +0100

    Create meanvsmedian.js

---
+++ codewars/meanvsmedian.js
@@ -0,0 +1,29 @@
+function meanVsMedian(numbers)
+{
+    let mean = numbers.sum / numbers.length;
+    let median = 0;
+    const mid = Math.floor(numbers.length / 2);
+    const sorted = numbers.sort((a, b) => a - b);
+
+    if (numbers.length % 2 === 0)
+    {
+        median = ((sorted[mid - 1] + sorted[mid]) / 2);
+    }
+    else
+    {
+        median = sorted[mid];
+    }
+
+    if (mean > median)
+    {
+        return("mean")
+    }
+    else if (median > mean)
+    {
+        return("median")
+    }
+    else if (median == mean)
+    {
+        return("same")
+    }
+}
