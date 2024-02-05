922451d6f03152512b09245413a9970f5fd069c5
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2024-02-05 15:32:20 +0100

    Update leonardo.js

--- codewars/leonardo.js
+++ codewars/leonardo.js
@@ -1,7 +1,6 @@
 const L = (n, L0, L1, add) => {
-    const result = [L0, L1]; // Initialize the array with initial values L0 and L1
+    const result = [L0, L1]; 
 
-    // Generate the remaining Leonardo numbers
     for (let i = 2; i < n; i++) {
         const nextNum = result[i - 1] + result[i - 2] + add;
         result.push(nextNum);
