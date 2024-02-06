0ea4ecff9ac3782a7bb928630bee71defe9e1f52
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2024-02-06 09:22:42 +0100

    Update allornothing.js

---
+++ dolgozat_02_06/allornothing.js
@@ -0,0 +1,33 @@
+// All or Nothing
+function possiblyPerfect(key, answers) {
+    let helyes = true;
+    let helytelen = true;
+  
+    for (let i = 0; i < key.length; i++) {
+      if (key[i] !== '_') {
+        if (key[i] !== answers[i]) {
+          helyes = false;
+        } else {
+          helytelen = false;
+        }
+      }
+    }
+  
+    return helyes || helytelen;
+}
+
+console.log(possiblyPerfect([..."A_C_B"],[..."ADCEB"]) + " >> true ");
+console.log( possiblyPerfect([..."B_B"],[..."BDC"]) + " >> false ");
+console.log( possiblyPerfect([..."T_FFF"],[..."FFTTT"]) + " >> true ");
+console.log( possiblyPerfect([..."BA__"],[..."BACC"]) + " >> true ");
+console.log( possiblyPerfect([..."ABA_"],[..."BACC"]) + " >> true ");
+console.log( possiblyPerfect([..."ABC_"],[..."BACC"]) + " >> false ");
+console.log( possiblyPerfect([..."B_"],[..."CA"]) + " >> true ");
+console.log( possiblyPerfect([..."BA"],[..."CA"]) + " >> false ");
+console.log( possiblyPerfect([..."B"],[..."B"]) + " >> true ");
+console.log( possiblyPerfect([..."_T__"],[..."TFFF"]) + " >> true ");
+console.log( possiblyPerfect([..."_T__"],[..."TTFT"]) + " >> true ");
+console.log( possiblyPerfect([..."_TTT"],[..."TTFT"]) + " >> false ");
+console.log( possiblyPerfect([..."_TTT"],[..."TTTT"]) + " >> true ");
+console.log( possiblyPerfect([..."_TTT"],[..."FFFF"]) + " >> true ");
+console.log( possiblyPerfect([..."____"],[..."FFFF"]) + " >> true ");
