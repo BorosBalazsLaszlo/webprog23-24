00f0bd83e1e1f2126b55a2be2a1ee79ef021d4ac
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2024-02-06 09:31:37 +0100

    Update swaphead.js

--- dolgozat_02_06/swaphead.js
+++ dolgozat_02_06/swaphead.js
@@ -1,1 +1,12 @@
+// Swap the head and the tail
+function swapHeadAndTail(arr) {
+    let kozepso = Math.floor(arr.lenght / 2);
+    let head = arr.slice(0, kozepso);
+    let tail = arr.slice(kozepso);
+    let kicserelt = [...tail, ...head]
+    return kicserelt
+}
 
+console.log(swapHeadAndTail([ 1, 2, 3, 4, 5 ]), [ 4, 5, 3, 1, 2 ]);
+console.log(swapHeadAndTail([ -1, 2 ]), [ 2, -1 ]);
+console.log(swapHeadAndTail([ 1, 2, -3, 4, 5, 6, -7, 8 ]), [ 5, 6, -7, 8, 1, 2, -3, 4 ]);
