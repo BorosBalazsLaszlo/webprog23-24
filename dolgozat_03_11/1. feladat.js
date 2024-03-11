cce09e6753bcfcbb8883d86bc545fbeaec956492
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2024-03-11 13:08:36 +0100

    Delete dolgozat_03_11 directory

--- dolgozat_03_11/1. feladat.js
+++
@@ -1,9 +0,0 @@
-function getNumberFromString(str)
-{
-    return [...str].filter(a => isNaN(a) == false).join("")
-}
-
-console.log("----- 1. feladat -----");
-console.log(getNumberFromString("1"), 1);
-console.log(getNumberFromString("123"), 123);
-console.log(getNumberFromString("this is number: 7"), 7);
