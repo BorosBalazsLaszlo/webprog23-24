2a5260ef1839f1516e3359ca32d313b500f04782
Boros Balázs László <94058470+BorosBalazsLaszlo@users.noreply.github.com>
2024-05-17 11:23:27 +0200

    Update catyears_dogyears.js

--- codewars/catyears_dogyears.js
+++ codewars/catyears_dogyears.js
@@ -1,18 +1,20 @@
 var humanYearsCatYearsDogYears = function(humanYears) {
-let catYears = 0;
+  let catYears = 0;
   let dogYears = 0;
 
   if (humanYears >= 1) {
-    catYears += 15;
-    dogYears += 15;
+    catYears = 15;
+    dogYears = 15;
   }
+
   if (humanYears >= 2) {
     catYears += 9;
     dogYears += 9;
   }
-  if (humanYears >= 3) {
-    catYears += 4 * (humanYears - 2);
-    dogYears += 5 * (humanYears - 2);
+
+  if (humanYears > 2) {
+    catYears += (humanYears - 2) * 4;
+    dogYears += (humanYears - 2) * 5;
   }
 
   return [humanYears, catYears, dogYears];
